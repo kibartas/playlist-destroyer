9a4d1f072044f00ece3dfb35a46d3f061b510e69
kibartas <juodasis.perlas@gmail.com>
2020-07-20 16:36:34 +0300

    add cypress and delete examples

---
+++ cypress/plugins/index.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+const execa = require('execa');
+const findBrowser = () => {
+  // the path is hard-coded for simplicity
+  const browserPath = '/usr/bin/brave';
+
+  return execa(browserPath, ['--version']).then((result) => {
+    // STDOUT will be like "Brave Browser 77.0.69.135"
+    const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(
+      result.stdout,
+    );
+    const majorVersion = parseInt(version.split('.')[0]);
+
+    return {
+      name: 'Brave',
+      channel: 'stable',
+      family: 'chromium',
+      displayName: 'Brave',
+      version,
+      path: browserPath,
+      majorVersion,
+    };
+  });
+};
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  return findBrowser().then((browser) => {
+    return {
+      browsers: config.browsers.concat(browser),
+    };
+  });
+};
