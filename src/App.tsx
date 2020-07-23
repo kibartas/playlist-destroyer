5a5434b98b9d92af1a2a9956c3d3cd1f21f57af5
kibartas <juodasis.perlas@gmail.com>
2020-07-23 14:13:12 +0300

    create routing to Login and NotFound pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React, { ReactElement } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
+import NotFound from './pages/NotFound';
 
 const App = (): ReactElement => (
-  <div>
-    <Login />
-  </div>
+  <Router>
+    <Switch>
+      <Route path="/" exact>
+        <Login />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
