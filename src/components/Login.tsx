0ba561fe19522cab8590b24f70c480ae7c72f491
kibartas <juodasis.perlas@gmail.com>
2020-08-28 20:08:54 +0300

    start using material ui and implement mock design for login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { AxiosResponse } from 'axios';
 import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
+import {
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
 import login from '../utils/login';
 
-const LoginButton = styled.input`
-  background: linear-gradient(90deg, #7d837c, #979c96);
-`;
-
-type FieldError = 'Required field' | '';
 type LoginError =
   | 'Invalid credentials'
   | 'Network error'
@@ -16,35 +18,40 @@ type LoginError =
   | 'An error occurred'
   | '';
 
+const useStyles = makeStyles({
+  root: {
+    border: 'solid 5px black',
+  },
+  textField: {
+    backgroundColor: 'white',
+  },
+  form: {
+    border: 'solid 2px orange',
+  },
+  button: {
+    alignSelf: 'center',
+  },
+  errorText: {
+    height: '30px',
+    fontSize: '30px',
+    color: 'red',
+    alignSelf: 'center',
+  },
+});
+
 const Login = (): React.ReactElement => {
+  const classes = useStyles();
+
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [usernameError, setUsernameError] = useState<FieldError>('');
-  const [passwordError, setPasswordError] = useState<FieldError>('');
   const [loginError, setLoginError] = useState<LoginError>('');
-  const inputEl = useRef<HTMLInputElement>(null);
 
   const history = useHistory();
-
-  useEffect(() => {
-    if (inputEl.current) {
-      inputEl.current.focus();
-    }
-  }, []);
 
   const handleSubmit = async (
     event: React.FormEvent<HTMLFormElement>,
   ): Promise<void> => {
     event.preventDefault();
-    if (username.length === 0 || password.length === 0) {
-      if (username.length === 0) {
-        setUsernameError('Required field');
-      }
-      if (password.length === 0) {
-        setPasswordError('Required field');
-      }
-      return;
-    }
     try {
       const { data } = await login({ username, password });
       setLoginError('');
@@ -72,42 +79,51 @@ const Login = (): React.ReactElement => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} data-testid="loginForm">
-        <span data-testid="errorMessage">{loginError}</span>
-        <label data-testid="usernameLabel" htmlFor="loginUsernameField">
-          {usernameError}
-          <br />
-          Username:
-          <input
-            id="loginUsernameField"
-            required
-            type="text"
-            data-testid="usernameText"
-            placeholder="e.g. JohnLukeThe3rd"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-            ref={inputEl}
-          />
-        </label>
-        <label data-testid="passwordLabel" htmlFor="loginPasswordField">
-          {passwordError}
-          <br />
-          Password:
-          <input
-            id="loginPasswordField"
-            required
-            type="password"
-            data-testid="passwordText"
-            placeholder="e.g. ••••••••••"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-        <br />
-        <LoginButton type="submit" data-testid="loginButton" value="Login" />
+    <Container className={classes.root} maxWidth="xl">
+      <form
+        className={classes.form}
+        onSubmit={handleSubmit}
+        data-testid="loginForm"
+      >
+        <Grid container direction="column" spacing={2} alignContent="center">
+          <Grid className={classes.errorText} item>
+            <Typography data-testid="errorMessage">{loginError}</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              className={classes.textField}
+              id="loginUsernameField"
+              required
+              type="text"
+              label="Username"
+              data-testid="usernameText"
+              placeholder="e.g. JohnLukeThe3rd"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              autoFocus
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              className={classes.textField}
+              id="loginPasswordField"
+              required
+              label="Password"
+              type="password"
+              data-testid="passwordText"
+              placeholder="e.g. ••••••••••"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </Grid>
+          <Grid item className={classes.button}>
+            <Button type="submit" data-testid="loginButton" variant="contained">
+              <Typography>Login</Typography>
+            </Button>
+          </Grid>
+        </Grid>
       </form>
-    </div>
+    </Container>
   );
 };
 
