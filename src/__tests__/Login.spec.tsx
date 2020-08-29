a91989679bbb6d725b03c9adecb8790fb249351f
kibartas <juodasis.perlas@gmail.com>
2020-08-29 08:26:35 +0300

    change Login.spec.tsx to abide best react-testing-library practices

--- src/__tests__/Login.spec.tsx
+++ src/__tests__/Login.spec.tsx
@@ -50,11 +50,8 @@ describe('<Login />', () => {
   describe('typing user info', () => {
     it('shows username and password', async () => {
       expect(screen.queryByDisplayValue('JohnLukeThe3rd')).toBeNull();
-      await userEvent.type(
-        screen.getByTestId('usernameText'),
-        'JohnLukeThe3rd',
-      );
-      await userEvent.type(screen.getByTestId('passwordText'), 'Password123');
+      await userEvent.type(screen.getByLabelText(/Username/), 'JohnLukeThe3rd');
+      await userEvent.type(screen.getByLabelText(/Password/), 'Password123');
       expect(screen.queryByDisplayValue('JohnLukeThe3rd')).toBeInTheDocument();
       expect(screen.queryByDisplayValue('Password123')).toBeInTheDocument();
     });
@@ -62,9 +59,7 @@ describe('<Login />', () => {
 
   describe('username field', () => {
     it('gets focused on component mount', () => {
-      expect(screen.getByTestId('usernameText')).toEqual(
-        document.activeElement,
-      );
+      expect(screen.getByLabelText(/Username/)).toEqual(document.activeElement);
     });
   });
 
@@ -74,31 +69,20 @@ describe('<Login />', () => {
     let form: HTMLElement;
 
     beforeEach(() => {
-      loginButton = screen.getByText(/login/i);
+      loginButton = screen.getByRole('button', { name: /login/i });
       submitHandler = jest.fn((e) => e.preventDefault());
-      form = screen.getByTestId(/loginForm/i);
+      form = screen.getByRole('form', { name: /login/i });
       form.onsubmit = submitHandler;
-    });
-
-    it('should display error if fields or a field is empty', () => {
-      userEvent.click(loginButton);
-      expect(submitHandler).toHaveBeenCalledTimes(1);
-      expect(screen.getByTestId(/usernameLabel/i)).toHaveTextContent(
-        'Required field',
-      );
-      expect(screen.getByTestId(/passwordLabel/i)).toHaveTextContent(
-        'Required field',
-      );
     });
 
     it('executes if fields non-empty', async () => {
       expect(submitHandler).not.toHaveBeenCalled();
       await userEvent.type(
-        screen.getByTestId('usernameText'),
+        screen.getByLabelText(/Username/),
         userData.username,
       );
       await userEvent.type(
-        screen.getByTestId('passwordText'),
+        screen.getByLabelText(/Password/),
         userData.password,
       );
       await act(async () => userEvent.click(loginButton));
@@ -110,11 +94,11 @@ describe('<Login />', () => {
   describe('login error label', () => {
     beforeEach(async () => {
       await userEvent.type(
-        screen.getByTestId('usernameText'),
+        screen.getByLabelText(/username/i),
         userData.username,
       );
       await userEvent.type(
-        screen.getByTestId('passwordText'),
+        screen.getByLabelText(/password/i),
         userData.password,
       );
     });
