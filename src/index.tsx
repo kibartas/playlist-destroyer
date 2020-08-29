b51d37cc0051959946baf23e5836183ca9f6da89
kibartas <juodasis.perlas@gmail.com>
2020-08-29 08:27:36 +0300

    createMuiTheme and wrap ThemeProvider around App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 import App from './App';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#28D761',
+    },
+    secondary: {
+      main: '#12131C',
+    },
+    info: {
+      main: '#C5C5C5',
+    },
+    error: {
+      main: '#FFA500',
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
