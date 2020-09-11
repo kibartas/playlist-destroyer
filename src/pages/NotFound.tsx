ade77cbe8487850d6e3d8f27380d8542817d98c9
kibartas <juodasis.perlas@gmail.com>
2020-09-11 13:53:22 +0300

    erroneous change

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 
 const NotFound = (): ReactElement => (
-  <h1 data-testid="notFoundMessage">Page not found</h1>
+  <h1 data-testid="notFoundMessage">Page not found </h1>
 );
 
 export default NotFound;
